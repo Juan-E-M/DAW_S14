dfd2e1fed5b079b8f13c5fd1ad4da5fe9e7af551
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2022-11-27 22:31:32 -0500

    lab12

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import PosPos from './modulos/pos/pages/PosPos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <PosPos/>
+  )
+}
+
+export default App
